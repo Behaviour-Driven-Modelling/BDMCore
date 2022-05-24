9f86e9b1ad2419fac31eb2a212d691518425cb19
mactanex <malthedj@hotmail.com>
2022-05-24 11:57:29 +0200

    Added initial project
    ########################################################################
    Why:
    To start the development of the BDM core libraries.
    
    ########################################################################
    How:
    Created a new Maven project
    
    ########################################################################
    Tags:
    
    ########################################################################
    Who:
    MDJ

---
+++ core/src/main/java/com/bdm/App.java
@@ -0,0 +1,13 @@
+package com.bdm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
